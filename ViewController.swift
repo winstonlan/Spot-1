801323032a0b7f10439c2efb498f84f3cf69874d
Winston Lan <winston.chunhua.lan@gmail.com>
2019-01-28 16:14:17 -0800

    Pod Install Firebase

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spot
+//
+//  Created by Winston Lan on 1/28/19.
+//  Copyright © 2019 Winston Lan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
